536ee668f0562bd6a72619969ef6d3d23c202724
boozedog <code@booze.dog>
2025-11-26 13:58:32 -0500

    some mbp-m3-pro stuff

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # nix-darwin
 
+## update
+
+```sh
+sudo darwin-rebuild switch --flake ~/projects/nix-darwin#mbp-m3-pro
+```
+
+## initial
+
 ```sh
 sudo determinate-nixd upgrade
 nix flake init --template "https://flakehub.com/f/DeterminateSystems/flake-templates/0#nix-darwin"
 sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake ~/projects/nix-darwin#mbp-m3-pro
-sudo darwin-rebuild switch --flake ~/projects/nix-darwin#mbp-m3-pro
 ```
--- mbp-m3-pro/configuration.nix
+++ mbp-m3-pro/configuration.nix
@@ -5,12 +5,18 @@
   environment = {
     systemPackages = with pkgs; [
       awk-language-server
-      direnv
+      #direnv
       dockerfile-language-server
+      git
+      git-extras
       nil
       nixd
       nixfmt
+      nodePackages.npm-check-updates
       nodePackages.vscode-json-languageserver
+      terminal-notifier
+      trippy
+      tmux
     ];
     shells = with pkgs; [
       fish
@@ -19,20 +25,6 @@
       EDITOR = "hx";
     };
   };
-
-  programs = {
-    fish.enable = true;
-    # nixvim = {
-    #   enable = true;
-    #   colorschemes.catppuccin.enable = true;
-    #   plugins = {
-    #     nvim-tree.enable = true;
-    #   	web-devicons.enable = true;
-    #   };
-    # };
-  };
-
-  services.tailscale.enable = true;
 
   fonts.packages = [
     pkgs.inter
@@ -44,43 +36,72 @@
     user = "david";
     brews = [
       "aichat"
+      "asitop"
       "bat"
       "btop"
       "chezmoi"
       #"container"
       "docker-compose-langserver"
+      "duf"
+      "eza"
       "fish-lsp"
+      "forgit"
+      "fzf"
+      #"gping" # using trippy now
       "helix"
       "lazydocker"
-      "lazygit"
+      #"lazygit" # using forgit now
       "opencode"
+      "pnpm"
       "prettier"
+      "ripgrep"
       "starship"
-      "tig"
+      #"tig" # using forgit now
       "tombi"
+      "topgrade"
       "typescript-language-server"
       "yaml-language-server"
-      "zellij"
+      #"zellij" # using tmux now
     ];
     casks = [
       # "alacritty"
       "alt-tab"
       "amethyst"
+      "beyond-compare"
       "brave-browser"
+      "claude"
       "claude-code"
       "google-chrome"
-      # "hyper"
-      # "kitty"
+      #"hyper" # using ghostty
+      #"kitty" # using ghostty
+      "localsend"
       "microsoft-edge"
+      "numi"
       "obsidian"
       "orbstack"
+      "postgres-unofficial"
       "ghostty"
       "ungoogled-chromium"
       "vscodium"
-      "warp"
-      # "wezterm"
+      #"warp" # nah
+      #"wezterm" # using ghostty
+      "yaak"
       "zed"
     ];
+  };
+
+  programs = {
+    direnv.enable = true;
+    fish.enable = true;
+    nix-index.enable = true;
+    # nixvim = {
+    #   enable = true;
+    #   colorschemes.catppuccin.enable = true;
+    #   plugins = {
+    #     nvim-tree.enable = true;
+    #   	web-devicons.enable = true;
+    #   };
+    # };
   };
 
   security.pam.services.sudo_local = {
@@ -88,14 +109,36 @@
     touchIdAuth = true;
   };
 
+  services = {
+    tailscale.enable = true;
+    openssh = {
+      enable = true;
+      extraConfig = ''
+        PasswordAuthentication no
+        PermitRootLogin no
+      '';
+    };
+  };
+
   system = {
     primaryUser = "root";
     configurationRevision = self.rev or self.dirtyRev or null;
     defaults = {
-      dock.expose-animation-duration = 0.1;
+      CustomUserPreferences = {
+        "com.apple.AppleMultitouchTrackpad" = {
+          ForceSuppressed = true;
+        };
+      };
+      dock = {
+        expose-animation-duration = 0.1;
+        magnification = true;
+        wvous-br-corner = 1;
+      };
       NSGlobalDomain = {
+        "com.apple.keyboard.fnState" = true;
         AppleShowAllExtensions = true;
         NSAutomaticWindowAnimationsEnabled = false;
+        NSWindowResizeTime = 0.001;
         NSAutomaticCapitalizationEnabled = false;
         NSAutomaticDashSubstitutionEnabled = false;
         NSAutomaticPeriodSubstitutionEnabled = false;
